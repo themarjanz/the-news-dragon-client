1ff2532d5c0b4ba16eb5060a8db9b77f32bf8d0d
themarjanz <themarjanz@gmail.com>
2023-09-02 21:12:01 +0600

    added react icons

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+import { FaUserCircle } from 'react-icons/fa'
 
 const Header = () => {
+    const style = { fontSize: "2rem" }
     return (
-        <div>
+        <Container>
             <div className='text-center'>
                 <img src={logo} alt="" />
                 <p className='text-secondry'><small>Journalism Without Fear or Favour</small></p>
@@ -14,7 +18,38 @@ const Header = () => {
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
 
-        </div>
+            {/* <div className='d-flex bg-light'> */}
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={50} >
+                    I can be a React component, multiple React components, or just some text....
+                    I can be a React component, multiple React components, or just some text....
+                </Marquee>
+            </div>
+
+
+            <Navbar collapseOnSelect expand="lg" bg="light" variant='light'>
+                <Container>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#carrier">Carrier</Nav.Link>
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets"><FaUserCircle style={style}></FaUserCircle></Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="secondary">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </Container>
     );
 };
 
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,33 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import Qzone from '../Qzone/Qzone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
+    const style = { color: "blue", fontSize: "1rem" }
+    const InstalStyle = { color: "red", fontSize: "1rem" }
     return (
         <div>
-            <h2>Right nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"> <FaGithub /> Login with Github</Button>
+
+            <div >
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook style={style}></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter style={style}></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram style={InstalStyle}></FaInstagram> Instagram</ListGroup.Item>
+
+                </ListGroup>
+            </div>
+
+            <Qzone></Qzone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
